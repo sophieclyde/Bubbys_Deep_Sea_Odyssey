a3ad3a3ed94f898c3f6c5f3e7aa0d23454023dd2
sophieclyde <sophieclyde@me.com>
2025-04-22 23:33:32 -0400

    Add files via upload
    Initial Build

---
+++ Bubby's Deep-Sea Odyssey/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "GA Tech Play _ Learn Lab-Bubby's Deep-Sea Odyssey-1.2";
+const contentToCache = [
+    "Build/Bubby's Deep-Sea Obyssey - April 2025 Build.loader.js",
+    "Build/Bubby's Deep-Sea Obyssey - April 2025 Build.framework.js.br",
+    "Build/Bubby's Deep-Sea Obyssey - April 2025 Build.data.br",
+    "Build/Bubby's Deep-Sea Obyssey - April 2025 Build.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
